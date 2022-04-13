44104770d531c39fb1487b613134a21fd1e81bee
tanvirAlamOmi <tanviralamomi@gmail.com>
2022-04-14 01:06:27 +0600

    router differerniate

--- server/router/app.router.js
+++ server/router/app.router.js
@@ -3,6 +3,7 @@ import ProductController from "../controller/product.controller.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => new ProductController().findAll(req, res));
+router.get("/", async (req, res) => res.send("Get the BEST pizza 🍕 at PITTA ❤️"));
+router.get("/get_products", async (req, res) => new ProductController().findAll(req, res));
 
 export default router;

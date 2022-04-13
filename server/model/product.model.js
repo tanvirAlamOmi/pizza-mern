3455132e7930e12c552f4343fe2d75be317bd1ac
tanvirAlamOmi <tanviralamomi@gmail.com>
2022-04-13 22:57:52 +0600

    product model created

---
+++ server/model/product.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema(
+    {
+        name : {type: String, required: true},
+        varients: [],
+        prices: [],
+        category: {type: String, required: true},
+        image: {type: String, required: true},
+        description: {type: String, required: true}
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const productModel = mongoose.model('products', productSchema);
+
+export default productModel;

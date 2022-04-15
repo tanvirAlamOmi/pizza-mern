1e5285b1a3c3e0a6dee9e73c0b39cf397e5c7b8d
tanvirAlamOmi <tanviralamomi@gmail.com>
2022-04-16 03:08:12 +0600

    login view setup

---
+++ client/src/pages/auth/LoginPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoginComponent from '../../components/auth/LoginComponent'
+
+export default function LoginPage() {
+  return (
+    <div>
+        <LoginComponent />
+    </div>
+  )
+}

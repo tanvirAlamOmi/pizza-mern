aa2af4160ebfda0479eafcad6d58773ad7329a4f
tanvirAlamOmi <tanviralamomi@gmail.com>
2022-04-18 05:04:36 +0600

    login added

--- client/src/store.js
+++ client/src/store.js
@@ -4,20 +4,26 @@ import thunk from 'redux-thunk';
 import {composeWithDevTools} from '@redux-devtools/extension';
 import { getAllProductsReducer } from './reducers/productReducer';
 import { cartReducer } from './reducers/cartReducer';
-import { registerCustomerReducer } from './reducers/customerReducer';
+import { registerCustomerReducer, loginCustomerReducer } from './reducers/customerReducer';
 
 
 const finalReducer = combineReducers({
     getAllProductsReducer,
     cartReducer,
-    registerCustomerReducer
+    registerCustomerReducer,
+    loginCustomerReducer
 })
 
 const cartItems = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+const currentCustomer = localStorage.getItem('currentCustomer') ? JSON.parse(localStorage.getItem('currentCustomer')) : null;
 
 const initialState = {
     cartReducer : {
         cartItems
+    },
+    
+    loginCustomerReducer : {
+        currentCustomer
     }
 };
 const composeEnhancers = composeWithDevTools({});
